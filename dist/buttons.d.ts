520e13e85f1c7f861bf89a106d22ad79432f06ff
なつき <i@ntk.me>
2019-04-03 08:44:04 +0000

    Update typescript declaration

--- dist/buttons.d.ts
+++ dist/buttons.d.ts
@@ -1,11 +1,11 @@
-declare module "github-buttons" {
-  export function render(options: {
-    "href": string;
-    "aria-label"?: string;
-    "title"?: string;
-    "data-icon"?: string;
-    "data-size"?: string;
-    "data-show-count"?: boolean;
-    "data-text"?: string;
-  } | HTMLAnchorElement, callback: (el: HTMLIFrameElement | HTMLSpanElement) => void): void;
+export interface GitHubButtonOptions {
+  "href": string;
+  "aria-label"?: string;
+  "title"?: string;
+  "data-icon"?: string;
+  "data-size"?: string;
+  "data-show-count"?: boolean;
+  "data-text"?: string;
 }
+
+export function render(options: GitHubButtonOptions | HTMLAnchorElement, callback: (el: HTMLIFrameElement | HTMLSpanElement) => void): void;
